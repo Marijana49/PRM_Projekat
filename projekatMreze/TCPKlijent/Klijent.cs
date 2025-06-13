651e046bd0497c4e2a5e51bf6ae4f664fb43aa25
Marijana49 <marijanazaric8@gmail.com>
2025-06-13 13:43:37 +0200

    commit

--- projekatMreze/TCPKlijent/Klijent.cs
+++ projekatMreze/TCPKlijent/Klijent.cs
@@ -3,8 +3,7 @@ using System.Net;
 using System.Net.Sockets;
 using System.IO;
 using System.Runtime.Serialization.Formatters.Binary;
-using System.Text;
-using Klase;
+using Klase; 
 
 namespace TCPKlijent
 {
@@ -14,10 +13,8 @@ namespace TCPKlijent
         {
             Socket klijentSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
             IPEndPoint serverEP = new IPEndPoint(IPAddress.Loopback, 50001);
-            byte[] buffer = new byte[1024];
-
             klijentSocket.Connect(serverEP);
-            Console.WriteLine("Klijent uspesno povezan sa serverom!");
+            Console.WriteLine("Klijent uspešno povezan sa serverom!");
 
             BinaryFormatter binaryFormatter = new BinaryFormatter();
 
@@ -25,13 +22,17 @@ namespace TCPKlijent
             {
                 Console.WriteLine("Unesite LBO: ");
                 int LBO = Convert.ToInt32(Console.ReadLine());
+
                 Console.WriteLine("Ime pacijenta: ");
                 string imePacijenta = Console.ReadLine();
+
                 Console.WriteLine("Prezime pacijenta: ");
                 string prezimePacijenta = Console.ReadLine();
+
                 Console.WriteLine("Adresa pacijenta: ");
                 string adresaPacijenta = Console.ReadLine();
-                Console.WriteLine("Izaberite  usugu: terapija, pregled, urgentna pomoc");
+
+                Console.WriteLine("Izaberite uslugu: terapija, pregled, urgentna pomoc");
                 string izbor = Console.ReadLine();
 
                 Pacijent pacijent = new Pacijent
@@ -40,25 +41,27 @@ namespace TCPKlijent
                     Ime = imePacijenta,
                     Prezime = prezimePacijenta,
                     Adresa = adresaPacijenta,
-                    VrstaZahteva = izbor;
+                    VrstaZahteva = izbor
+                };
 
                 using (MemoryStream ms = new MemoryStream())
                 {
                     binaryFormatter.Serialize(ms, pacijent);
                     byte[] data = ms.ToArray();
-
                     klijentSocket.Send(data);
                 }
 
                 Console.WriteLine("Podaci o pacijentu su poslati!");
 
+                Console.WriteLine("Da li želite da unesete još jednog pacijenta? (da/ne)");
+                string odgovor = Console.ReadLine().ToLower();
+                if (odgovor != "da")
+                    break;
             }
 
-            Console.WriteLine("Klijent zavrsava sa radom");
-            Console.ReadKey();
+            Console.WriteLine("Klijent završava sa radom.");
             klijentSocket.Close();
-
+            Console.ReadKey();
         }
     }
 }
-
